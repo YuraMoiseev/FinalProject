4645acd025048fab037cbef2da9b2c58bb02646e
Ymois <Ymoiseev2007@gmail.com>
2024-11-18 11:49:12 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from mido import MidiFile
+from mido import *
+import editdistance
 # note off/on - type
 
 # 96 - one quarter
@@ -86,6 +87,50 @@ def Helps(arr1: list, arr2: list):
 
 def ClosestTiming():
     pass
+
+
+def manhattan_distance(list1, list2):
+    distance = 0
+    for sub1, sub2 in zip(list1, list2):
+        sub_distance = 0
+        for x, y in zip(sub1, sub2):
+            sub_distance += abs(x - y)
+        distance += sub_distance
+    return distance
+
+
+def compare_midi_files2(file1, file2):
+    # Load midi files
+    mid1 = MidiFile(file1)
+    mid2 = MidiFile(file2)
+
+    # Extract notes from midi files and group adjacent pitches together
+    notes1 = []
+    for msg in merge_tracks(mid1.tracks):
+        if 'note_on' in msg.type:
+            pitch = msg.note
+            if notes1 and pitch == notes1[-1][-1] + 1:
+                # Append pitch to last group
+                notes1[-1].append(pitch)
+            else:
+                # Create new group for pitch
+                notes1.append([pitch])
+
+    notes2 = []
+    for msg in merge_tracks(mid2.tracks):
+        if 'note_on' in msg.type:
+            pitch = msg.note
+            if notes2 and pitch == notes2[-1][-1] + 1:
+                # Append pitch to last group
+                notes2[-1].append(pitch)
+            else:
+                # Create new group for pitch
+                notes2.append([pitch])
+
+    # Calculate similarity for each group of pitches
+    similarity_scores = []
+    return manhattan_distance(notes1, notes2)
+
 
 
 print(CompareSequences("Temp1.mid", "Temp2.mid", 11))
